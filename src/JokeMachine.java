41abb964024ec02bb47288f8bf41bfb98e3309a7
aishani831 <37873747+aishani831@users.noreply.github.com>
2018-03-31 22:48:24 -0400

    Updated JokeMachine to tell jokes randomly

--- src/JokeMachine.java
+++ src/JokeMachine.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
 
 public class JokeMachine {
-
 	public static void main(String[] args) {
+		String[] jokeArray = new String[10];
+		jokeArray[0] = "Joke 1";
+		jokeArray[1] = "Joke 2";
+		jokeArray[2] = "Joke 3";
+		jokeArray[3] = "Joke 4";
+		jokeArray[4] = "Joke 5";
+		jokeArray[5] = "Joke 6";
+		jokeArray[6] = "Joke 7";
+		jokeArray[7] = "Joke 8";
+		jokeArray[8] = "Joke 9";
+		jokeArray[9] = "Joke 10";
 		Scanner sc = new Scanner(System.in);
-		String x = "";
-		int joke = (int) (Math.random()*10);
+		String input = "";
+		int jokeNumber = (int) (Math.random()*9);
 		System.out.println("Do you want to hear a joke?");
-		x = sc.next();
-		int i = sc.nextInt();
-		if (x.equals("yes")) {
-			System.out.println( i);
+		input = sc.next();
+		if (input.equals("yes")) {
+			System.out.println(jokeArray[jokeNumber]);
 		}
-		else if (x.equals("no")) {
+		else if (input.equals("no")) {
 			System.out.println("You will hear a joke anyways.");
-			System.out.println(i);
+			System.out.println(jokeArray[jokeNumber]);
 		}
 	}
 
